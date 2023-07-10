e0fc8890a4c9e6a624530df7045a2b28de39263c
tefujimaki <77235516+tefu0421@users.noreply.github.com>
2023-07-10 16:26:26 +0900

    rooting

--- src/index.js
+++ src/index.js
@@ -1,30 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Root from "./Root";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+import HomeRoot from "./HomeRoot";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<html>
-			<head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossorigin
-				/>
-				<link
-					//rel="preload"
-					href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100;200;300;400;500&display=block"
-					rel="stylesheet"
-				/>
-			</head>
-			<body>
-				<Root />
-			</body>
-		</html>
-	</React.StrictMode>
+	<BrowserRouter>
+		<Routes>
+			<Route exact path={"/"} element={<HomeRoot />} />
+		</Routes>
+		<Link to="/">Back To Top</Link>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
