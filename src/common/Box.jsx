8bbefe26d7dece09401871b5f95c1e98274ff87c
tefujimaki <tefujimaki@gmail.com>
2023-07-14 03:04:49 +0900

    Boxをクリックして移動する

--- src/common/Box.jsx
+++ src/common/Box.jsx
@@ -1,8 +1,15 @@
 import "./Box.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function Box(props) {
+	const navigate = useNavigate();
 	return (
-		<div class="Box" href={props.url}>
+		<div
+			class="Box"
+			onClick={() => {
+				navigate(props.url);
+			}}
+		>
 			<p>{props.title}</p>
 		</div>
 	);
