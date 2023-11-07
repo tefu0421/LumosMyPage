bc87485beaec6dadc094353d65bdc9c40347d064
tefujimaki <tefujimaki@gmail.com>
2023-11-07 14:27:56 +0900

    update packages

--- src/error/ErrorRoot.jsx
+++ src/error/ErrorRoot.jsx
@@ -1,21 +1,33 @@
 import Header from "../common/Header";
 import "./ErrorRoot.scss";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 export default function ErrorRoot() {
+	const number = useRef();
+	const message = useRef();
+
+	useEffect(() => {
+		gsap.from(number.current, {
+			y: +100,
+			ease: "elastic.out(0.8,0.0)",
+			delay: 0.1,
+		});
+		gsap.to(message.current, {});
+	}, []);
+
 	return (
 		<html>
 			<body>
-				<div>
-					<Header />
-					<div>
-						<p id="number">
-							<span>404</span>
-						</p>
-						<p id="message">
-							<span>page not found</span>
-						</p>
+				<Header />
+				<div id="content">
+					<div id="number">
+						<p ref={number}>404</p>
 					</div>
 				</div>
+				<p id="message">
+					<span>page not found</span>
+				</p>
 			</body>
 		</html>
 	);
